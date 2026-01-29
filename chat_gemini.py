976246fe414a9c486230e507a42abc0854e91370
anasds0816 <anas.ds0816@gmail.com>
2026-01-29 12:01:56 +0530

    Update chat_gemini.py

--- chat_gemini.py
+++ chat_gemini.py
@@ -1,1 +1,16 @@
+from google import genai
+from google.genai import types
 
+client = genai.Client(api_key='AIzaSyDAI6Z1QMN9hCGc1YuZqEhh8keNXV0tzmQ')
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents={'text': 'Why is the sky blue?'},
+    config={
+        'temperature': 0,
+        'top_p': 0.95,
+        'top_k': 20,
+    },
+)
+
+print(response.text)
